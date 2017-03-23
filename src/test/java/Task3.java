ac5bd50c91f4def605547f19e815cd2262f2e969
slayer <no@no.com>
2017-03-23 18:48:51 +0200

    naming fix

--- src/test/java/Task3.java
+++ src/test/java/Task3.java
@@ -18,7 +18,7 @@ public class Task3 {
     }
 
     @Test
-    public void T() {
+    public void Task3Test() {
         driver.get("http://localhost/litecart/admin/login.php");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
