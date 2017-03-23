07cf5784603bd6062323ccf3988568ca273d1c9a
slayer <no@no.com>
2017-03-23 18:44:56 +0200

    task 3

---
+++ src/test/java/Task3.java
@@ -0,0 +1,33 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+
+public class Task3 {
+
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void T() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
