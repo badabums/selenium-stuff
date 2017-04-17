234b41cd740284e7ab2c0eea2a2098170245d20c
Bogdan Lykhyi <bogdan.lykhyi@globallogic.com>
2017-04-17 20:02:51 +0300

    task 10, stubs

---
+++ src/test/java/Task10/pages/CheckoutPage.java
@@ -0,0 +1,7 @@
+package Task10.pages;
+
+/**
+ * Created by bogdan.lykhyi on 4/13/2017.
+ */
+public class CheckoutPage {
+}
---
+++ src/test/java/Task10/tests/AddProductTest.java
@@ -0,0 +1,7 @@
+package Task10.tests;
+
+/**
+ * Created by bogdan.lykhyi on 4/13/2017.
+ */
+public class AddProductTest {
+}
